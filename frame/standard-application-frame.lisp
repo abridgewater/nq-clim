a2813d67563368434c033ac6f7d85370e04f4954
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2014-12-31 10:41:45 -0500

    frame/standard-application-frame: Standard frames should be manageable.
      * If someone wants to define an unmanageable frame (or wants to
    implement their own frame management interface), that's fine, but
    the standard is manageability.

--- frame/standard-application-frame.lisp
+++ frame/standard-application-frame.lisp
@@ -7,12 +7,14 @@
 (cl:defpackage :nq-clim/frame/standard-application-frame
   (:use :cl
         :nq-clim/frame/application-frame
-        :nq-clim/frame/application-frame-functions)
+        :nq-clim/frame/application-frame-functions
+        :nq-clim/frame/manageable-frame-mixin)
   (:export
    "STANDARD-APPLICATION-FRAME"))
 (cl:in-package :nq-clim/frame/standard-application-frame)
 
-(defclass standard-application-frame (application-frame)
+(defclass standard-application-frame (application-frame
+                                      manageable-frame-mixin)
   ((pretty-name :initarg :pretty-name :accessor frame-pretty-name)))
 
 ;;; EOF
