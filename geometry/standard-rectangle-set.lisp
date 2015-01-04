5e9308773b69d9eeaa2ce57b741949e040e6c7cc
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-01-04 17:54:59 -0500

    geometry/standard-rectangle-set: More on composite region unions.
      * Implement the UNION operation for sets of rectangular regions,
    with test cases.
    
      * This was tricky enough to get working, and also tricky to
    simplify the code in order for the core algorithm to appear
    reasonably concise.  I'm not completely convinced that the
    tradeoff for the latter was worth it.

--- geometry/standard-rectangle-set.lisp
+++ geometry/standard-rectangle-set.lisp
@@ -55,4 +55,99 @@ X-spans representing the union of both sets."
          and do (setf start-1 start-2
                       end-1 end-2))))
 
+(defmacro collecting-y-spans (nil &body body)
+  "Return the set of Y-spans accumulated by calling (COLLECT-Y-SPAN
+START END X-SPANS) within BODY, coalescing identical adjacent
+Y-spans."
+  (let ((collected-spans-var (gensym))
+        (start-var (gensym))
+        (end-var (gensym))
+        (x-spans-var (gensym))
+        (finish-y-span-fun (gensym)))
+    `(let (,collected-spans-var
+           ,start-var
+           ,end-var
+           ,x-spans-var)
+       (labels ((,finish-y-span-fun ()
+                  (when ,start-var
+                    (push (cons (cons ,start-var ,end-var) ,x-spans-var)
+                          ,collected-spans-var)))
+                (collect-y-span (start end x-spans)
+                  (cond
+                    ((and ,end-var
+                          (= start ,end-var)
+                          (equal x-spans ,x-spans-var))
+                     (setf ,end-var end))
+                    (t
+                     (,finish-y-span-fun)
+                     (setf ,start-var start
+                           ,end-var end
+                           ,x-spans-var x-spans)))))
+         (progn
+           ,@body)
+         (,finish-y-span-fun)
+         (nreverse ,collected-spans-var)))))
+
+(defun unite-y-span-sets (set-1 set-2)
+  "Given two sets of Y-spans, each in order, produce a minimal set of
+Y-spans representing the union of both sets."
+  (collecting-y-spans ()
+    ;; This mess, from the first MACROLET through the LET, allows us
+    ;; to work with two Y-spans, each drawn from their specific list,
+    ;; being able to refer to each Y-span in aggregate as SPAN-[12] or
+    ;; in terms of the components START-[12], END-[12], and
+    ;; X-SPANS-[12], to retrieve the next Y-span using NEXT-SPAN on
+    ;; the corresponding list variable SET-[12], to be able to mutate
+    ;; each Y-span without affecting the original data (the input sets
+    ;; are treated as immutable), and to treat SPAN-{START,END,SET} as
+    ;; accessors.  Arguably, it is an exercise in excess cleverness.
+    (macrolet ((span-start (span)
+                 (ecase span (span-1 'start-1) (span-2 'start-2)))
+               (span-end (span)
+                 (ecase span (span-1 'end-1) (span-2 'end-2)))
+               (span-set (span)
+                 (ecase span (span-1 'set-1) (span-2 'set-2)))
+               (next-span (set)
+                 `(let ((span (pop ,set)))
+                    (values (caar span) (cdar span) (cdr span)))))
+      (symbol-macrolet ((span-1 (values start-1 end-1 x-spans-1))
+                        (span-2 (values start-2 end-2 x-spans-2)))
+        (let (start-1 end-1 x-spans-1 start-2 end-2 x-spans-2)
+          (macrolet ((discard-span-before (span boundary)
+                       ;; If SPAN extends beyond BOUNDARY, discard the
+                       ;; portion of SPAN prior to BOUNDARY.
+                       ;; Otherwise, pull the next span from the
+                       ;; corresponding list.
+                       `(if (<= (span-end ,span) ,boundary)
+                            (setf ,span (next-span (span-set ,span)))
+                            (setf (span-start ,span) ,boundary))))
+
+            (setf span-1 (next-span set-1))
+            (setf span-2 (next-span set-2))
+            (loop
+               while (and span-1 span-2)
+               do (cond
+                    ((< start-1 start-2)
+                     (collect-y-span start-1 (min end-1 start-2) x-spans-1)
+                     (discard-span-before span-1 start-2))
+
+                    ((> start-1 start-2)
+                     (collect-y-span start-2 (min end-2 start-1) x-spans-2)
+                     (discard-span-before span-2 start-1))
+
+                    (t
+                     (let ((boundary (min end-1 end-2)))
+                       (collect-y-span start-1 boundary
+                                       (unite-x-span-sets x-spans-1 x-spans-2))
+                       (discard-span-before span-1 boundary)
+                       (discard-span-before span-2 boundary)))))
+            (loop
+               while span-1
+               do (collect-y-span start-1 end-1 x-spans-1)
+                 (setf span-1 (next-span set-1)))
+            (loop
+               while span-2
+               do (collect-y-span start-2 end-2 x-spans-2)
+                 (setf span-2 (next-span set-2)))))))))
+
 ;;; EOF
--- tests/standard-rectangle-set-tests.lisp
+++ tests/standard-rectangle-set-tests.lisp
@@ -7,7 +7,8 @@
 (cl:defpackage :nq-clim/tests/standard-rectangle-set-tests
   (:use :cl :sb-rt)
   (:import-from :nq-clim/geometry/standard-rectangle-set
-                "UNITE-X-SPAN-SETS"))
+                "UNITE-X-SPAN-SETS"
+                "UNITE-Y-SPAN-SETS"))
 (cl:in-package :nq-clim/tests/standard-rectangle-set-tests)
 
 
@@ -50,5 +51,137 @@
     (unite-x-span-sets '((14 . 27) (40 . 61))
                      '((20 . 40)))
   ((14 . 61)))
+
+
+(deftest (unite-y-span-sets same-regions)
+    (unite-y-span-sets '(((25 . 50) (20 . 30)))
+                       '(((25 . 50) (20 . 30))))
+  (((25 . 50) (20 . 30))))
+
+(deftest (unite-y-span-sets disjoint-horizontal-regions set-1-first)
+    (unite-y-span-sets '(((25 . 50) (20 . 30)))
+                       '(((25 . 50) (40 . 50))))
+  (((25 . 50) (20 . 30) (40 . 50))))
+
+(deftest (unite-y-span-sets disjoint-horizontal-regions set-2-first)
+    (unite-y-span-sets '(((25 . 50) (40 . 50)))
+                       '(((25 . 50) (20 . 30))))
+  (((25 . 50) (20 . 30) (40 . 50))))
+
+(deftest (unite-y-span-sets abutting-horizontal-regions)
+    (unite-y-span-sets '(((25 . 50) (20 . 30)))
+                       '(((25 . 50) (30 . 40))))
+  (((25 . 50) (20 . 40))))
+
+(deftest (unite-y-span-sets overlapping-horizontal-regions)
+    (unite-y-span-sets '(((25 . 50) (20 . 30)))
+                       '(((25 . 50) (25 . 35))))
+  (((25 . 50) (20 . 35))))
+
+(deftest (unite-y-span-sets enclosed-with-overlap set-1-first)
+    (unite-y-span-sets '(((25 . 100) (25 . 100)))
+                       '(((50 . 75) (50 . 75))))
+  (((25 . 100) (25 . 100))))
+
+(deftest (unite-y-span-sets enclosed-with-overlap set-2-first)
+    (unite-y-span-sets '(((50 . 75) (50 . 75)))
+                       '(((25 . 100) (25 . 100))))
+  (((25 . 100) (25 . 100))))
+
+(deftest (unite-y-span-sets overlapping-vertical-regions set-1-first)
+    (unite-y-span-sets '(((25 . 50) (25 . 50)))
+                       '(((45 . 70) (25 . 50))))
+  (((25 . 70) (25 . 50))))
+
+(deftest (unite-y-span-sets overlapping-vertical-regions set-2-first)
+    (unite-y-span-sets '(((45 . 70) (25 . 50)))
+                       '(((25 . 50) (25 . 50))))
+  (((25 . 70) (25 . 50))))
+
+(deftest (unite-y-span-sets abutting-vertical-regions set-1-first)
+    (unite-y-span-sets '(((25 . 50) (25 . 50)))
+                       '(((50 . 75) (25 . 50))))
+  (((25 . 75) (25 . 50))))
+
+(deftest (unite-y-span-sets abutting-vertical-regions set-2-first)
+    (unite-y-span-sets '(((50 . 75) (25 . 50)))
+                       '(((25 . 50) (25 . 50))))
+  (((25 . 75) (25 . 50))))
+
+(deftest (unite-y-span-sets disjoint-vertical-regions set-1-first)
+    (unite-y-span-sets '(((25 . 50) (25 . 50)))
+                       '(((74 . 99) (25 . 50))))
+  (((25 . 50) (25 . 50))
+   ((74 . 99) (25 . 50))))
+
+(deftest (unite-y-span-sets disjoint-vertical-regions set-2-first)
+    (unite-y-span-sets '(((74 . 99) (25 . 50)))
+                       '(((25 . 50) (25 . 50))))
+  (((25 . 50) (25 . 50))
+   ((74 . 99) (25 . 50))))
+
+(deftest (unite-y-span-sets overlapping-squares set-1-northwest)
+    (unite-y-span-sets '(((25 . 75) (25 . 75)))
+                       '(((50 . 100) (50 . 100))))
+  (((25 . 50) (25 . 75))
+   ((50 . 75) (25 . 100))
+   ((75 . 100) (50 . 100))))
+
+(deftest (unite-y-span-sets overlapping-squares set-1-northeast)
+    (unite-y-span-sets '(((25 . 75) (50 . 100)))
+                       '(((50 . 100) (25 . 75))))
+  (((25 . 50) (50 . 100))
+   ((50 . 75) (25 . 100))
+   ((75 . 100) (25 . 75))))
+
+(deftest (unite-y-span-sets overlapping-squares set-1-southeast)
+    (unite-y-span-sets '(((50 . 100) (50 . 100)))
+                       '(((25 . 75) (25 . 75))))
+  (((25 . 50) (25 . 75))
+   ((50 . 75) (25 . 100))
+   ((75 . 100) (50 . 100))))
+
+(deftest (unite-y-span-sets overlapping-squares set-1-southwest)
+    (unite-y-span-sets '(((50 . 100) (25 . 75)))
+                       '(((25 . 75) (50 . 100))))
+  (((25 . 50) (50 . 100))
+   ((50 . 75) (25 . 100))
+   ((75 . 100) (25 . 75))))
+
+(deftest (unite-y-span-sets cross-shape set-1-vertical)
+    (unite-y-span-sets '(((25 . 100) (50 . 75)))
+                       '(((50 . 75) (25 . 100))))
+  (((25 . 50) (50 . 75))
+   ((50 . 75) (25 . 100))
+   ((75 . 100) (50 . 75))))
+
+(deftest (unite-y-span-sets cross-shape set-2-vertical)
+    (unite-y-span-sets '(((50 . 75) (25 . 100)))
+                       '(((25 . 100) (50 . 75))))
+  (((25 . 50) (50 . 75))
+   ((50 . 75) (25 . 100))
+   ((75 . 100) (50 . 75))))
+
+(deftest (unite-y-span-sets checkerboard set-1-northwest)
+    (unite-y-span-sets '(((25 . 50) (25 . 50))
+                         ((50 . 75) (50 . 75)))
+                       '(((25 . 50) (50 . 75))
+                         ((50 . 75) (25 . 50))))
+  (((25 . 75) (25 . 75))))
+
+(deftest (unite-y-span-sets checkerboard set-2-northwest)
+    (unite-y-span-sets '(((25 . 50) (50 . 75))
+                         ((50 . 75) (25 . 50)))
+                       '(((25 . 50) (25 . 50))
+                         ((50 . 75) (50 . 75))))
+  (((25 . 75) (25 . 75))))
+
+(deftest (unite-y-span-sets cross-shape double-horizontal-with-covered-gaps)
+    (unite-y-span-sets '(((25 . 100) (50 . 75)))
+                       '(((50 . 60) (25 . 50) (60 . 100))
+                         ((60 . 75) (25 . 60) (75 . 100))))
+  (((25 . 50) (50 . 75))
+   ((50 . 75) (25 . 100))
+   ((75 . 100) (50 . 75))))
 
 ;;; EOF
