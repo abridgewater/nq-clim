eb5f823a54a0c77242536831496d722f0cf281e7
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-01-08 21:32:47 -0500

    geometry/nowhere: Initial rough-in, the empty region.
      * This is the region comprising of absolutely no points.  It is
    unbounded, and has no dimensions.  The main thing to note is that
    it has a privileged "singleton" instance which is a constant (as
    per CLIM II 3.1).
    
      * Here we establish the class, the constant instance, print-read
    consistency, and externalizability of literal uses in compiled
    files.
    
      * We do not yet add it to the build order, as I have yet to
    fully sort out what its dependencies are and what it depends on,
    especially as we move towards the region predicate and region
    composition protocols.

---
+++ geometry/nowhere.lisp
@@ -0,0 +1,45 @@
+;;;
+;;; nq-clim/geometry/nowhere.lisp
+;;;
+;;; The empty region (from CLIM II 3.1)
+;;;
+
+(cl:defpackage :nq-clim/geometry/nowhere
+  (:use :cl
+        :nq-clim/geometry/region)
+  (:export
+   "+NOWHERE+"))
+(cl:in-package :nq-clim/geometry/nowhere)
+
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  ;; The NOWHERE class and corresponding MAKE-LOAD-FORM method both
+  ;; need to be available at compile-time in order to set up +NOWHERE+
+  ;; properly.
+  (defclass nowhere (region) ())
+
+  ;; Any references to an instance of NOWHERE are to be dumped as a
+  ;; symbol value access to +NOWHERE+ (as a singleton instance).
+  (defmethod make-load-form ((object nowhere) &optional environment)
+    (declare (ignore environment))
+    '(symbol-value +nowhere+)))
+
+;; If this is the first definition of +NOWHERE+, create an instance of
+;; NOWHERE to use.  Otherwise, use the existing value.  This stunt is
+;; usually (and illegally) pulled for non-EQL "constant" values.  In
+;; this case, we're making sure that the value will always be EQ, even
+;; through print/read and compile-file/load.
+(defconstant +nowhere+ (if (boundp '+nowhere+)
+                           (symbol-value '+nowhere+)
+                           (make-instance 'nowhere))
+  "The empty region, which contains no points and has no bounding
+  rectangle.")
+
+;; When printing a NOWHERE, make it evaluate to the value of
+;; +NOWHERE+, for print/read consistency.
+(defmethod print-object ((object nowhere) stream)
+  (if (and *print-readably* (not *read-eval*))
+      (error 'print-not-readable :object object)
+      (format stream "#.~S" '+nowhere+)))
+
+;;; EOF
