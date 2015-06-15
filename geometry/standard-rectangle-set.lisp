2e7bccf56c63ebd761457aeec37e3b5aabfc3579
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-06-14 20:27:26 -0400

    geometry/standard-rectangle-set: Implement the STANDARD-RECTANGLE-SET class
      * Currently largely a value-holder for the y-span-sets "shape"
    structure, and a BOUNDING-RECTANGLE* method to find the bounds
    from such a "shape".
    
      * The bouding rectangle would be a lot more straightforward to
    compute while building up the "shape" in the first place, so we
    may move towards doing so in the future, but this should work for
    the time being.

--- geometry/standard-rectangle-set.lisp
+++ geometry/standard-rectangle-set.lisp
@@ -5,9 +5,11 @@
 ;;;
 
 (cl:defpackage :nq-clim/geometry/standard-rectangle-set
-  (:use :cl)
+  (:use :cl
+        :nq-clim/geometry/bounding-rectangle-protocol
+        :nq-clim/geometry/region-set)
   (:export
-   ))
+   "STANDARD-RECTANGLE-SET"))
 (cl:in-package :nq-clim/geometry/standard-rectangle-set)
 
 ;; We represent a rectangle-set as a "shape" (Foley, van Dam, Feiner,
@@ -235,5 +237,20 @@ Y-spans representing the intersection of both sets."
   "Given two sets of Y-spans, each in order, produce a minimal set of
 Y-spans representing the difference of both sets."
   (operate-on-y-span-sets set-1 set-2 #'difference-operation))
+
+
+(defclass standard-rectangle-set (region-set)
+  ((y-spans :initarg y-spans :reader region-y-spans)))
+
+(defmethod bounding-rectangle* ((region standard-rectangle-set))
+  ;; Unfortunately, this relies on intimate knowledge of the structure
+  ;; of the SHAPE held in Y-SPANS.  It is also quite thoroughly
+  ;; opaque.
+  (let* ((y-spans (region-y-spans region))
+         (min-x (apply #'min (mapcar #'caadr y-spans)))
+         (min-y (caaar y-spans))
+         (max-x (apply #'max (mapcar #'cdar (mapcar #'last y-spans))))
+         (max-y (cdaar (last y-spans))))
+    (values min-x min-y max-x max-y)))
 
 ;;; EOF
