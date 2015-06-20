620901ab731ebed4e049de7a13b39c4b7f7563f8
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-06-20 12:09:58 -0400

    clim-sys/named-constant-mixin: A mixin for the common "constant class" idiom
      * Already in NQ-CLIM we have three cases where a singleton
    instance of some class is defined to be a constant.  The actual
    implementation of these cases is somewhat verbose...  and in one
    case, inconsistent.  And we have at least eleven more in the
    offing.
    
      * Define a mixin class and a macro to paper over almost all of
    the work involved in this idiom.  The idea is to use the mixin as
    part of the singleton (or flyweight) class, then use the macro to
    define the actual constants.  The class definition still needs to
    be made available at compile-time, and any documentation of the
    constant needs to be applied afterwards, but a lot of the
    boilerplate just goes away.

---
+++ clim-sys/named-constant-mixin.lisp
@@ -0,0 +1,44 @@
+;;;
+;;; nq-clim/clim-sys/named-constant-mixin.lisp
+;;;
+;;; Support for defining constants that are CLOS instances.
+;;;
+
+(cl:defpackage :nq-clim/clim-sys/named-constant-mixin
+  (:use :cl)
+  (:export
+   "DEFINE-NAMED-CONSTANT"
+   "NAMED-CONSTANT-MIXIN"))
+(cl:in-package :nq-clim/clim-sys/named-constant-mixin)
+
+(defclass named-constant-mixin ()
+  ((constant-name :initarg constant-name
+                  :reader named-constant-mixin-constant-name)))
+
+;; Any references to an instance of NAMED-CONSTANT-MIXIN are to be
+;; dumped as a symbol value access to the named constant (as a
+;; singleton instance).
+(defmethod make-load-form ((object named-constant-mixin) &optional environment)
+  (declare (ignore environment))
+  `(symbol-value ,(named-constant-mixin-constant-name object)))
+
+;; When printing a NAMED-CONSTANT-MIXIN, make it evaluate to the value
+;; of the name, for print/read consistency.
+(defmethod print-object ((object named-constant-mixin) stream)
+  (if (and *print-readably* (not *read-eval*))
+      (error 'print-not-readable :object object)
+      (format stream "#.~S" (named-constant-mixin-constant-name object))))
+
+
+;; If this is the first definition of the constant, create an instance
+;; of CLASS to use.  Otherwise, use the existing value.  This stunt is
+;; usually (and illegally) pulled for non-EQL "constant" values.  In
+;; this case, we're making sure that the value will always be EQ, even
+;; through print/read and compile-file/load.
+(defmacro define-named-constant (name class &rest initargs)
+  `(defconstant ,name
+     (if (boundp ',name)
+         (symbol-value ',name)
+       (apply #'make-instance ',class 'constant-name ',name ,initargs))))
+
+;;; EOF
