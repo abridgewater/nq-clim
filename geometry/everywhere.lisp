15c71abf307f89b7a7a49723c5e137e1bc581cae
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-06-20 12:19:01 -0400

    geometry/everywhere: Convert to NAMED-CONSTANT-MIXIN
      * Not much to say here.

--- geometry/everywhere.lisp
+++ geometry/everywhere.lisp
@@ -6,6 +6,7 @@
 
 (cl:defpackage :nq-clim/geometry/everywhere
   (:use :cl
+        :nq-clim/clim-sys/named-constant-mixin
         :nq-clim/geometry/region
         :nq-clim/geometry/transformation-protocol)
   (:export
@@ -14,34 +15,15 @@
 
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  ;; The EVERYWHERE class and corresponding MAKE-LOAD-FORM method both
-  ;; need to be available at compile-time in order to set up
-  ;; +EVERYWHERE+ properly.
-  (defclass everywhere (region) ())
+  ;; The EVERYWHERE class needs to be available at compile-time in
+  ;; order to set up +EVERYWHERE+ properly.
+  (defclass everywhere (region named-constant-mixin) ()))
 
-  ;; Any references to an instance of EVERYWHERE are to be dumped as a
-  ;; symbol value access to +EVERYWHERE+ (as a singleton instance).
-  (defmethod make-load-form ((object everywhere) &optional environment)
-    (declare (ignore environment))
-    '(symbol-value +everywhere+)))
+(define-named-constant +everywhere+ everywhere)
 
-;; If this is the first definition of +EVERYWHERE+, create an instance
-;; of EVERYWHERE to use.  Otherwise, use the existing value.  This
-;; stunt is usually (and illegally) pulled for non-EQL "constant"
-;; values.  In this case, we're making sure that the value will always
-;; be EQ, even through print/read and compile-file/load.
-(defconstant +everywhere+ (if (boundp '+everywhere+)
-                           (symbol-value '+everywhere+)
-                           (make-instance 'everywhere))
+(setf (documentation '+everywhere+ 'variable)
   "The all-inclusive region, which contains all points and has no
 bounding rectangle.")
-
-;; When printing a EVERYWHERE, make it evaluate to the value of
-;; +EVERYWHERE+, for print/read consistency.
-(defmethod print-object ((object everywhere) stream)
-  (if (and *print-readably* (not *read-eval*))
-      (error 'print-not-readable :object object)
-      (format stream "#.~S" '+everywhere+)))
 
 
 ;; Transformations apply to points within a region, and while
