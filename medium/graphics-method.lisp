c40a5469227caa7da97c5a4871c0490f29dfc102
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-06-27 21:48:40 -0400

    medium/graphics-method: Initial rough-in for basic drawing functions
      * The basics for DRAW-POINT*, DRAW-LINE*, and friends.
    
      * Two issues here.  First, the spec breaks down when it comes to
    rectangles and polygons, with respect to the CLOSED and FILLED
    keyword arguments.  Second, there is a clear need for these
    functions (and others) to accept keyword arguments to pass to the
    underlying MEDIUM- functions.
    
      * There is also an opportunity for excellence, in limiting the
    drawing options accepted per graphics-method to those that make
    sense for the graphics-method, and setting up the arglist hinting
    appropriately.
    
      * But there's definitely a mess here to clean up before we get
    as far as actually rendering text.

--- medium/graphics-method.lisp
+++ medium/graphics-method.lisp
@@ -5,9 +5,18 @@
 ;;;
 
 (cl:defpackage :nq-clim/medium/graphics-method
-  (:use :cl)
+  (:use :cl
+        :nq-clim/medium/drawing
+        :nq-clim/medium/drawing-options)
   (:export
-   "DEFINE-GRAPHICS-METHOD"))
+   "DEFINE-GRAPHICS-METHOD"
+   "DRAW-POINT*"
+   "DRAW-POINTS*"
+   "DRAW-LINE*"
+   "DRAW-LINES*"
+   "DRAW-POLYGON*"
+   "DRAW-RECTANGLE*"
+   "DRAW-RECTANGLES*"))
 (cl:in-package :nq-clim/medium/graphics-method)
 
 
@@ -19,5 +28,28 @@
        (apply #'invoke-with-drawing-options
               medium #'thunk drawing-options))))
 
+
+(define-graphics-method draw-point*
+    medium-draw-point* (x y))
+(define-graphics-method draw-points*
+    medium-draw-points* (coord-seq))
+(define-graphics-method draw-line*
+    medium-draw-line* (x1 y1 x2 y2))
+(define-graphics-method draw-lines*
+    medium-draw-lines* (coord-seq))
+
+ ;; FIXME: The CLOSED and FILLED arguments below are supposed to be
+ ;; keywords.  Or aren't supposed to be here at all in the case of
+ ;; DRAW-RECTANGLES* But they aren't specified as parameters at all
+ ;; for the corresponding MEDIUM- functions in the spec, nor are they
+ ;; specified as medium properties...  And it turns out that some
+ ;; other drawing functions that we'll need later require other
+ ;; keyword arguments.
+(define-graphics-method draw-polygon*
+    medium-draw-polygon* (coord-seq closed filled))
+(define-graphics-method draw-rectangle*
+    medium-draw-rectangle* (x1 y1 x2 y2 filled))
+(define-graphics-method draw-rectangles*
+    medium-draw-rectangles* (coord-seq filled))
 
 ;;; EOF
