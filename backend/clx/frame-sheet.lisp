06085315d48d18ff6720dc33e8ec62555ba9fb7a
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-07-10 10:16:23 -0400

    backend/clx/frame-sheet: Allow CLX-FRAME-SHEET to adopt child sheets
      * Not quite an oversight in the original design, given its
    history, but since we already have code to conditionally invoke
    SHEET-ADOPT-CHILD with a CLX-FRAME-SHEET as the parent, it'd be
    nice if it didn't break because of something so painfully obvious.

--- backend/clx/frame-sheet.lisp
+++ backend/clx/frame-sheet.lisp
@@ -6,14 +6,18 @@
 
 (cl:defpackage :nq-clim/backend/clx/frame-sheet
   (:use :cl
+        :nq-clim/sheet/basic-sheet
         :nq-clim/sheet/mirrored-sheet-mixin
-        :nq-clim/sheet/basic-sheet)
+        :nq-clim/sheet/sheet-multiple-child-mixin)
   (:export
    "CLX-FRAME-SHEET"
    "CLX-FRAME-SHEET-FRAME"))
 (cl:in-package :nq-clim/backend/clx/frame-sheet)
 
 (defclass clx-frame-sheet (mirrored-sheet-mixin
+                           ;; KLUDGE: "Should" be
+                           ;; SHEET-SINGLE-CHILD-MIXIN
+                           sheet-multiple-child-mixin
                            basic-sheet)
   ((frame :initarg :frame :accessor clx-frame-sheet-frame)))
 
