dfcd5f22fc3fbfb1aa9c0d81dbcdacfd8b208e84
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-07-23 22:46:23 -0400

    medium/standard-line-style: A default implementation of the line-style protocol
      * STANDARD-LINE-STYLE is a required class, implementing the
    protocol for LINE-STYLE objects.  Entirely slots with :READERs.
    
      * No way to create one of these yet, but soon.

---
+++ medium/standard-line-style.lisp
@@ -0,0 +1,24 @@
+;;;
+;;; nq-clim/medium/standard-line-style
+;;;
+;;; The default implementation of the LINE-STYLE protocol
+;;;
+
+(cl:defpackage :nq-clim/medium/standard-line-style
+  (:use :cl
+        :nq-clim/medium/line-style
+        :nq-clim/medium/line-style-protocol)
+  (:export
+   "STANDARD-LINE-STYLE"))
+(cl:in-package :nq-clim/medium/standard-line-style)
+
+
+(defclass standard-line-style (line-style)
+  ((unit :initarg unit :reader line-style-unit)
+   (thickness :initarg thickness :reader line-style-thickness)
+   (join-shape :initarg joint-shape :reader line-style-joint-shape)
+   (cap-shape :initarg cap-shape :reader line-style-cap-shape)
+   (dashes :initarg dashes :reader line-style-dashes)))
+
+
+;;; EOF
