0e7ebaa63bf8ec8122dc3629a97ea19e5316682c
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-07-24 19:55:47 -0400

    medium/standard-line-style: Implement MAKE-LINE-STYLE
      * This is the defined interface for creating STANDARD-LINE-STYLE
    instances.  Defaults are specified, and types declared as well as
    possible using the standard type system (which doesn't allow for
    types such as "even-lengthed sequence of real").

--- medium/standard-line-style.lisp
+++ medium/standard-line-style.lisp
@@ -9,7 +9,8 @@
         :nq-clim/medium/line-style
         :nq-clim/medium/line-style-protocol)
   (:export
-   "STANDARD-LINE-STYLE"))
+   "STANDARD-LINE-STYLE"
+   "MAKE-LINE-STYLE"))
 (cl:in-package :nq-clim/medium/standard-line-style)
 
 
@@ -20,5 +21,22 @@
    (cap-shape :initarg cap-shape :reader line-style-cap-shape)
    (dashes :initarg dashes :reader line-style-dashes)))
 
+(defun make-line-style (&key (unit :normal) (thickness 1) (joint-shape :miter) (cap-shape :butt) (dashes nil))
+  (declare (type (member :normal :point :coordinate) unit)
+           (type (real 0) thickness)
+           (type (member :miter :bevel :round :none) joint-shape)
+           (type (member :butt :square :round :no-end-point) cap-shape)
+           (type (or sequence null (eql t)) dashes))
+  ;; FIXME: DASHES is supposed to be an even-lengthed sequence of
+  ;; REALs (plausibly positive REALs), NIL (which is actually an
+  ;; even-lengthed sequence containing no elements), or T...  But
+  ;; we're only verifying that it's a SEQUENCE or T, not the length or
+  ;; element constraints.
+  (make-instance 'standard-line-style
+                 'unit unit
+                 'thickness thickness
+                 'joint-shape joint-shape
+                 'cap-shape cap-shape
+                 'dashes dashes))
 
 ;;; EOF
