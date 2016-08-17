f3535b1ce1e7a9e9e46ad0d213963841ab164a59
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2016-08-17 11:46:01 -0400

    backend/clx/frame-sheet: Tie the frame-sheet into the input protocol
      * Two pieces here.  The first is simply that the frame-sheet
    needs to handle input, thus needs to include the standard input
    mixin.  The second is that this is the highest non-graft mirror
    for the application, and thus the natural place to terminate our
    search for the per-frame event queue, so we just put a slot on the
    class hold the queue and set up SHEET-EVENT-QUEUE as a reader,
    overriding the (less-specific) method on the standard input mixin.

--- backend/clx/frame-sheet.lisp
+++ backend/clx/frame-sheet.lisp
@@ -6,9 +6,11 @@
 
 (cl:defpackage :nq-clim/backend/clx/frame-sheet
   (:use :cl
+        :nq-clim/event/event-queue-protocol
         :nq-clim/sheet/basic-sheet
         :nq-clim/sheet/mirrored-sheet-mixin
-        :nq-clim/sheet/sheet-multiple-child-mixin)
+        :nq-clim/sheet/sheet-multiple-child-mixin
+        :nq-clim/sheet/standard-sheet-input-mixin)
   (:export
    "CLX-FRAME-SHEET"
    "CLX-FRAME-SHEET-FRAME"))
@@ -18,7 +20,9 @@
                            ;; KLUDGE: "Should" be
                            ;; SHEET-SINGLE-CHILD-MIXIN
                            sheet-multiple-child-mixin
+                           standard-sheet-input-mixin
                            basic-sheet)
-  ((frame :initarg :frame :accessor clx-frame-sheet-frame)))
+  ((frame :initarg :frame :accessor clx-frame-sheet-frame)
+   (event-queue :initarg :event-queue :reader sheet-event-queue)))
 
 ;;; EOF
